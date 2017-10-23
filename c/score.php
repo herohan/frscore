79d4d07273a935fd2053ecf3ad412656cff554b7
EC2 Default User <ec2-user@ip-172-31-8-57.ap-northeast-2.compute.internal>
2017-10-23 15:20:37 +0000

    check clear penalty

--- c/score.php
+++ c/score.php
@@ -2,7 +2,7 @@
 
 require_once("../i/common.inc");
 
-$hands_1 = array(1, 2, 3, 4, 5, 6, 7);
+$hands_1 = array(1, 2, 3, 4, 13, 6, 16);
 CalcHands($hands_1)
 //print("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><br>\n");
 //print_r($user_card);
