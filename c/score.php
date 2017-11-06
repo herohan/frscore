762adbd005d1a1d04f11acc11e04c5cb3b8efe85
EC2 Default User <ec2-user@ip-172-31-8-57.ap-northeast-2.compute.internal>
2017-11-06 13:57:16 +0000

    add withany

--- c/score.php
+++ c/score.php
@@ -2,7 +2,9 @@
 
 require_once("../i/rule.inc");
 
-$hands_1 = array(1, 2, 3, 4, 13, 6, 16);
+print("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><br>\n");
+
+$hands_1 = array(27, 2, 3, 4, 13, 6, 16);
 CalcHands($hands_1)
 //print("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><br>\n");
 //print_r($user_card);
